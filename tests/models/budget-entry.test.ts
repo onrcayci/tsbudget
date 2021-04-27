82576d246aada304a818094201d16248f662d8e5
Onur Cayci <onur.cayci@mail.mcgill.ca>
2021-04-27 15:23:44 -0400

    Release v0.0.1 (#21)
    * Initialize TypeScript App
    
    * Initialize src folder and main file
    
    * Initialize typescript package
    
    * Add typescript build commands; Update typescript build configs
    
    * Add node types
    
    * Initialize BudgetEntry class
    
    * Update main.ts to test BudgetEntry class implementation
    
    * Add jest dependencies for testing
    
    * Initialize jest tests
    
    * Fix jest syntax error
    
    * Remove unused ts-jest dependency
    
    * Add babel dependency to properly run TJest tests that are written in TypeScript
    
    * Update mock.test.ts to budget-entry.test.ts to test the BudgetEntry class
    
    * Update tsconfig.json to comment out rootDir option
    
    * Implement save function for BudgetEntry model, which saves the object into a json file
    
    * Update tests to clean up any save files after the test run is complete; Instantiate the used variable once to decrease code repetition
    
    * Implement update method; update save method to use synchronous file system methods
    
    * Update test suite to conform with the constructor changes; Update test suite to use synchronous file system methods
    
    * turn _parseEntries method to private parseEntries; Update update method to properly update entry details; Implement update method successful testcase
    
    * Update update method implementation to be a static method; implement helper private method updateEntry
    
    * Remove description from BudgetEntry class; add list and delete methods to the BudgetEntry class; update npm start script and add npm dev script; update main to implement CLI application using yargs
    
    * Add todo comments to the BudgetEntry class
    
    * Update README
    
    * Implement balance method
    
    * Add more TODO comments as a reminder
    
    * Update .gitignore
    
    * Update unit tests to cover BudgetEntry mclass; Update BudgetEntry class to behave according to the expected test results; Update main to due to changed BudgetEntry class behaviour
    
    * UUpdate the documentation of the code
    
    * Implement method to list entries based on the year and month entered by the user
    
    * Update node dependencies
    
    * Add TSDoc for the 'listByMonth' method
    
    * Add documentation to 'balanceByMonth' method; Update 'balance' CLI command to support 'balanceByMonth' method; Update naming of option 'yearMonth' to 'time'
    
    * Implement tests, Update exception handling and catch major bug
    Implement 'listByMonth' method tests
    Update exception handling to decrease repetition
    Catch date bug where depending on the date string the date object becomes time aware
    
    * {Feature} Implement tests (#16)
    
    * Update date related descriptions to set the standard input dates as the UTC format to fix a major date related bug
    
    * Implement all unit tests for the BudgetEntry class
    
    * Implement GitHub Actions (#17)
    
    * Implement GitHub Actions
    
    * Update Node.js versions used for CI
    
    * {Feature} README (#18)
    
    * Implement GitHub Actions
    
    * Update YNode.js versions used for CI
    
    * Update README - Add setup section
    
    * Remove .DS_Store
    
    * Update package.json and tsconfig.json for release of the app (#20)

---
+++ tests/models/budget-entry.test.ts
@@ -0,0 +1,106 @@
+import { existsSync, readFileSync, rmSync } from "fs";
+
+import { BudgetEntry } from "../../src/models/budget-entry";
+
+let  testEntry: BudgetEntry;
+
+beforeAll(() => {
+    testEntry = new BudgetEntry({
+        title: "Test Entry",
+        amount: 100,
+        currency: "CAD",
+        recurring: false,
+        date: "2021-02-01"
+    });
+});
+
+afterEach(() => {
+    if (existsSync("save_file.json")) rmSync("save_file.json");
+});
+
+test("Successfully create an instance of BudgetEntry class", () => {
+    expect(testEntry).toBeInstanceOf(BudgetEntry);
+    expect(testEntry.title).toEqual("Test Entry");
+    expect(testEntry.amount).toEqual(100);
+    expect(testEntry.currency).toEqual("CAD");
+    expect(testEntry.date).toEqual("2021-02-01");
+    expect(testEntry.recurring).toBeFalsy();
+});
+
+test("Successfully save a created BudgetEntry instance", () => {
+    testEntry.save();
+    const savedEntries: BudgetEntry[] = JSON.parse(readFileSync("save_file.json", { encoding: "utf8" }));
+    expect(savedEntries.length).toEqual(1);
+    expect(savedEntries[0].title).toEqual("Test Entry");
+    expect(savedEntries[0].amount).toEqual(100);
+    expect(savedEntries[0].currency).toEqual("CAD");
+    expect(savedEntries[0].date).toEqual("2021-02-01");
+    expect(savedEntries[0].recurring).toBeFalsy();
+});
+
+test("Sucessfully update a saved BudgetEntry instance", () => {
+    testEntry.save();
+    BudgetEntry.update(testEntry.title, {
+        title: "Updated Title"
+    });
+    const savedEntries: BudgetEntry[] = JSON.parse(readFileSync("save_file.json", { encoding: "utf8" }));
+    expect(savedEntries.length).toEqual(1);
+    expect(savedEntries[0].title).toEqual("Updated Title");
+});
+
+test("Fail to update a BudgetEntry which is not saved", () => {
+    expect(() => BudgetEntry.update(testEntry.title, { title: "Updated Title" })).toThrow("There are no saved entries!");
+})
+
+test("Successfully delete a saved BudgetEntry instance", () => {
+    testEntry.save();
+    BudgetEntry.delete(testEntry.title);
+    const savedEntries: BudgetEntry[] = JSON.parse(readFileSync("save_file.json", { encoding: "utf8" }));
+    expect(savedEntries.length).toEqual(0);
+});
+
+test("Fail to delete a BudgetEntry which is not saved", () => {
+    expect(() => BudgetEntry.delete(testEntry.title)).toThrow("There are no saved entries!");
+});
+
+test("Successfully list all saved BudgetEntry instances", () => {
+    testEntry.save();
+    const savedEntries: BudgetEntry[] = BudgetEntry.list();
+    expect(savedEntries.length).toEqual(1);
+    expect(savedEntries[0]).toEqual(testEntry);
+});
+
+test("Fail to list BudgetEntry instances since they are not saved", () => {
+    expect(() => BudgetEntry.list()).toThrow("There are no saved entries!");
+});
+
+test("Successfully return the current total expense", () => {
+    testEntry.save();
+    const balance: number = BudgetEntry.balance();
+    expect(balance).toEqual(testEntry.amount);
+});
+
+test("Fail to return the current total expense due to not having a save file", () => {
+    expect(() => BudgetEntry.balance()).toThrow("There are no saved entries!");
+});
+
+test("Successfully list all of the entries from a given year and month", () => {
+    testEntry.save();
+    const entries: BudgetEntry[] = BudgetEntry.listByMonth("2021-02");
+    expect(entries.length).toEqual(1);
+    expect(entries[0].date).toEqual(testEntry.date);
+});
+
+test("Fail to return the list of all entries from a given year and month to due not having a save file", () => {
+    expect(() => BudgetEntry.listByMonth("2021-02")).toThrow("There are no saved entries!");
+});
+
+test("Sucessfully return the total expense of a given time period", () => {
+    testEntry.save();
+    const balanceByMonth = BudgetEntry.balanceByMonth("2021-02");
+    expect(balanceByMonth).toEqual(testEntry.amount);
+});
+
+test("Fail to return the total expense of a given time period due to not having a save file", () => {
+    expect(() => BudgetEntry.balanceByMonth("2021-02")).toThrow("There are no saved entries!");
+});
